0ae864af6dfdaca219df6706842d3e9d78b247cd
Sukum Duangpattra <tonypengfusama@gmail.com>
2020-12-24 00:46:17 +0700

    ProcessInput Added. Unsuccessful Discrete Collision Response

--- SDL_Collision/main.cpp
+++ SDL_Collision/main.cpp
@@ -116,6 +116,9 @@ public:
     void SetDirection(const Vector2D& dir) { m_direction = dir; }
     Vector2D GetDirection() { return m_direction.Normalized(); }
     
+    float GetWidth() { return m_width;}
+    float GetHeight() {return m_height;}
+    
     float GetMin_X() { return  m_position.x - m_width/2; }
     float GetMax_X() { return  m_position.x + m_width/2; }
     float GetMin_Y() { return  m_position.y + m_height/2; }
@@ -174,8 +177,11 @@ void StayWithinFrame(GameObject* go, float left, float right, float top, float b
     }
 }
 
-bool CheckCollision(GameObject* A, GameObject* B)
+bool CheckCollision(GameObject* A, GameObject* B, Vector2D* previousPos)
 {
+    
+    std::cout << "A postion x: " << A->GetPosition().x << std::endl;
+    
     bool isCollided;
     
     bool overlap_x;
@@ -199,17 +205,19 @@ bool CheckCollision(GameObject* A, GameObject* B)
         B->IsCollided() = true;
         return isCollided;
     }else{
+        *previousPos = A->GetPosition();
+        std::cout<< "previouspos: " << A->GetPosition().x << std::endl;
         isCollided = false;
         A->IsCollided() = false;
         B->IsCollided() = false;
         return isCollided;
     }
-    
-
 }
 
 void OnCollisionResponse(GameObject* A, GameObject* B){
-    if(CheckCollision(A, B)){
+    Vector2D previousPos;
+    
+    if(CheckCollision(A, B, &previousPos)){
         if(A->IsCollided()){
 
             if(A->GetMax_X() > B->GetMin_X() &&
@@ -237,6 +245,88 @@ void OnCollisionResponse(GameObject* A, GameObject* B){
     }
 }
 
+void OnDiscreteCollisionResponse(GameObject* A, GameObject* B, Vector2D& previousPos){
+   
+    float A_bottom = previousPos.y + A->GetHeight()/2;
+    float A_top    = previousPos.y - A->GetHeight()/2;
+    float A_left   = previousPos.x - A->GetWidth()/2;
+    float A_right  = previousPos.x + A->GetWidth()/2;
+    std::cout<< " A_right " << A_right << std::endl;
+    std::cout<< "B min x "  << B->GetMin_X() << std::endl;
+    
+    bool HitOnleft = false;
+    bool HitOnBottom = false;
+    
+
+    if (A_right < B->GetMin_X()) {
+        HitOnleft = true;
+    }else{
+        HitOnleft = false;
+    }
+    
+    if ((previousPos.x + A->GetWidth()/2 > B->GetMin_X() ||
+        previousPos.x - A->GetWidth()/2 < B->GetMax_X()) &&
+        previousPos.y - A->GetHeight()/2 > B->GetMin_Y()){
+        HitOnBottom = true;
+    }
+    
+    if (A->IsCollided()) {
+         
+        
+        
+        if (HitOnleft) {
+            if(A->GetMax_X() > B->GetMin_X() &&
+               A->GetMin_X() < B->GetMin_X()){
+                A->SetPosition(Vector2D(B->GetMin_X() - A->GetWidth()/2, A->GetPosition().y));
+            }
+        }
+//        if(A->GetMin_X() < B->GetMax_X() &&
+//           A->GetMax_X() > B->GetMax_X()){
+//            A->SetPosition(Vector2D(B->GetMax_X() + A->GetWidth()/2, A->GetPosition().y));
+//        }
+        if (HitOnBottom) {
+            if(A->GetMax_Y() < B->GetMin_Y() &&
+               A->GetMin_Y() > B->GetMin_Y()){
+                A->SetPosition(Vector2D(A->GetPosition().x, B->GetMin_Y() + A->GetHeight()/2));
+            }
+        }
+        std::cout<< "HitOnLeft" << HitOnleft << std::endl;
+        std::cout<< "HitOnBottom" << HitOnBottom << std::endl;
+
+//        if(A->GetMin_Y() > B->GetMax_Y() &&
+//           A->GetMax_Y() < B->GetMax_Y()){
+//            A->SetPosition(Vector2D(A->GetPosition().x, B->GetMax_Y() - A->GetHeight()/2));
+//        }
+    }
+}
+
+void ProcessInput(GameObject* go, const Uint8* keyStates){
+    if(keyStates[SDL_SCANCODE_LEFT]){
+        go->SetDirection(Vector2D(-1,0));
+        go->SetSpeed(100);
+        go->Move(GetDeltaTime());
+        
+    }
+    if(keyStates[SDL_SCANCODE_RIGHT]){
+        go->SetDirection(Vector2D(1,0));
+        go->SetSpeed(100);
+        go->Move(GetDeltaTime());
+    }
+    if(keyStates[SDL_SCANCODE_UP]){
+        go->SetDirection(Vector2D(0,-1));
+        go->SetSpeed(100);
+        go->Move(GetDeltaTime());
+    }
+    if(keyStates[SDL_SCANCODE_DOWN]){
+        go->SetDirection(Vector2D(0,1));
+        go->SetSpeed(100);
+        go->Move(GetDeltaTime());
+    }
+   
+}
+
+
+
 int main(int argc, const char * argv[]) {
     
     if(!SDL_Init(SDL_INIT_VIDEO))
@@ -256,14 +346,30 @@ int main(int argc, const char * argv[]) {
     go_A->SetPosition(Vector2D(50, 100));
     go_A->SetDirection(Vector2D(1, 1));
     go_A->SetSpeed(120);
+    
     GameObject* go_B = new GameObject(renderer);
     go_B->SetSize(150, 150);
     go_B->SetPosition(Vector2D(250, 140));
     go_B->SetDirection(Vector2D(-1, 1));
     go_B->SetSpeed(0);
     
-    gameObjectPool.push_back(go_A);
+    GameObject* go_C = new GameObject(renderer);
+    go_C->SetSize(100, 100);
+    go_C->SetPosition(Vector2D(200, 200));
+    go_C->SetSpeed(0);
+
+    
+    
+   // gameObjectPool.push_back(go_A);
     gameObjectPool.push_back(go_B);
+    gameObjectPool.push_back(go_C);
+    
+    bool KEYS[512];
+    for(int i = 0; i < 512; ++i){
+        KEYS[i] = false;
+    }
+    
+   
     
     bool GameRunning = true;
     while (GameRunning) {
@@ -274,15 +380,46 @@ int main(int argc, const char * argv[]) {
             case SDL_QUIT:
                 GameRunning = false;
                 break;
+            case SDL_KEYDOWN:
+                break;
+            case SDL_KEYUP:
+//                go_C->SetDirection(Vector2D(0,0));
+//                go_C->SetSpeed(0);
+                break;
         }
         
-        for(auto go: gameObjectPool){
-            go->Move(GetDeltaTime());
-            StayWithinFrame(go, 0, 640, 0, 480);
+        const Uint8* keystates = SDL_GetKeyboardState(NULL);
+        
+        ProcessInput(go_C, keystates);
+        
+
+        if(go_C->GetMax_X() > SCREEN_WIDTH){
+            go_C->SetPosition(Vector2D(SCREEN_WIDTH - go_C->GetWidth()/2, go_C->GetPosition().y));
         }
+        if(go_C->GetMin_X() < 0){
+            go_C->SetPosition(Vector2D(go_C->GetWidth()/2, go_C->GetPosition().y));
+        }
+        if (go_C->GetMax_Y() < 0) {
+            go_C->SetPosition(Vector2D(go_C->GetPosition().x, go_C->GetHeight()/2));
+        }
+        if (go_C->GetMin_Y() > SCREEN_HEIGHT) {
+            go_C->SetPosition(Vector2D(go_C->GetPosition().x, SCREEN_HEIGHT - go_C->GetHeight()/2));
+        }
+        
+        
+        std::cout<< "GO_A position X:" << go_C->GetPosition().x << std::endl;
+        std::cout<< "GO_A position Y:" << go_C->GetPosition().y << std::endl;
+//        for(auto go: gameObjectPool){
+//            go->Move(GetDeltaTime());
+//            StayWithinFrame(go, 0, 640, 0, 480);
+//        }
            
-        OnCollisionResponse(go_A, go_B);
-        OnCollisionResponse(go_B, go_A);
+//        OnCollisionResponse(go_A, go_B);
+//        OnCollisionResponse(go_B, go_A);
+        
+        Vector2D previousPos;
+        CheckCollision(go_C, go_B, &previousPos);
+        OnDiscreteCollisionResponse(go_C, go_B, previousPos);
         
         go_A->OnCollided();
         go_B->OnCollided();
